be3b365351bd4a824b87fc8c0602697804c2eb64
Akintayo Popoola <akinpope25@gmail.com>
2025-07-27 18:41:37 -0500

    Added index and show pages with multi-model data and About page with ERD

--- app/controllers/wards_controller.rb
+++ app/controllers/wards_controller.rb
@@ -1,13 +1,22 @@
 class WardsController < ApplicationController
+  # Display a list of all wards
   def index
+    @wards = Ward.all
   end
 
+  # Display a single ward and its associated data
   def show
+    @ward = Ward.find(params[:id])
+    @service_requests = @ward.service_requests
+    @expense_reports = @ward.expense_reports
+    @events = @ward.events
   end
 
   def new
+    @ward = Ward.new
   end
 
   def edit
+    @ward = Ward.find(params[:id])
   end
 end
