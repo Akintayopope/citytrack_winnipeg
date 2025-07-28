134af9a6e32b3e811db50d5a159e0f806ee047a0
Akintayo Popoola <akinpope25@gmail.com>
2025-07-27 19:20:42 -0500

    Add ExpenseReportsController with search and pagination

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -1,17 +1,58 @@
-def index
-  @wards = Ward.all
-  @expense_reports = ExpenseReport.all
+class ExpenseReportsController < ApplicationController
+  before_action :set_expense_report, only: %i[show edit update destroy]
 
-  if params[:search].present?
-    @expense_reports = @expense_reports.where(
-      "category LIKE ?",
-      "%#{params[:search]}%"
-    )
+  def index
+    @wards = Ward.all
+    @expense_reports = ExpenseReport.all
+
+    if params[:search].present?
+      @expense_reports = @expense_reports.where("category LIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:ward_id].present?
+      @expense_reports = @expense_reports.where(ward_id: params[:ward_id])
+    end
+
+    @expense_reports = @expense_reports.page(params[:page]).per(10) # Pagination
   end
 
-  if params[:ward_id].present?
-    @expense_reports = @expense_reports.where(ward_id: params[:ward_id])
+  def show; end
+
+  def new
+    @expense_report = ExpenseReport.new
   end
 
-  @expense_reports = @expense_reports.page(params[:page]).per(10) # Pagination
+  def edit; end
+
+  def create
+    @expense_report = ExpenseReport.new(expense_report_params)
+    if @expense_report.save
+      redirect_to @expense_report, notice: 'Expense report was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @expense_report.update(expense_report_params)
+      redirect_to @expense_report, notice: 'Expense report was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @expense_report.destroy
+    redirect_to expense_reports_url, notice: 'Expense report was successfully deleted.'
+  end
+
+  private
+
+  def set_expense_report
+    @expense_report = ExpenseReport.find(params[:id])
+  end
+
+  def expense_report_params
+    params.require(:expense_report).permit(:ward_id, :category, :amount, :report_date)
+  end
 end
--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -1,18 +1,62 @@
-def index
-  @wards = Ward.all
-  @service_requests = ServiceRequest.all
+class ServiceRequestsController < ApplicationController
+  before_action :set_service_request, only: %i[show edit update destroy]
 
-  if params[:search].present?
-    @service_requests = @service_requests.where(
-      "category LIKE ? OR description LIKE ?",
-      "%#{params[:search]}%",
-      "%#{params[:search]}%"
-    )
+  def index
+    @wards = Ward.all
+    @service_requests = ServiceRequest.all
+
+    if params[:search].present?
+      @service_requests = @service_requests.where(
+        "category LIKE ? OR description LIKE ?",
+        "%#{params[:search]}%",
+        "%#{params[:search]}%"
+      )
+    end
+
+    if params[:ward_id].present?
+      @service_requests = @service_requests.where(ward_id: params[:ward_id])
+    end
+
+    @service_requests = @service_requests.page(params[:page]).per(10) # Kaminari pagination
   end
 
-  if params[:ward_id].present?
-    @service_requests = @service_requests.where(ward_id: params[:ward_id])
+  def show; end
+
+  def new
+    @service_request = ServiceRequest.new
   end
 
-  @service_requests = @service_requests.page(params[:page]).per(10) # Pagination
+  def edit; end
+
+  def create
+    @service_request = ServiceRequest.new(service_request_params)
+    if @service_request.save
+      redirect_to @service_request, notice: "Service request was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @service_request.update(service_request_params)
+      redirect_to @service_request, notice: "Service request was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @service_request.destroy
+    redirect_to service_requests_url, notice: "Service request was successfully deleted."
+  end
+
+  private
+
+  def set_service_request
+    @service_request = ServiceRequest.find(params[:id])
+  end
+
+  def service_request_params
+    params.require(:service_request).permit(:ward_id, :category, :description, :status, :request_date)
+  end
 end
